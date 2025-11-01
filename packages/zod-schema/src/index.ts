2fb8a7a864d56aee4670a553a73bdda405dd061d
Kenura R. Gunarathna <kenuragunarathna@gmail.com>
2025-11-01 22:34:57 +0530

    added zod-schema

--- packages/zod-schema/src/index.ts
+++ packages/zod-schema/src/index.ts
@@ -1,6 +1,6 @@
 export * from './data-tables'
 export * from './auth'
-// export * from './user' // DEPRECATED: Use auth.ts for User types (from better-auth)
+
 export * from './config'
 
 // tRPC schemas
