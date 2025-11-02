c82516e4773d7d723317b0bb519b2ba95509adad
Kenura R. Gunarathna <kenuragunarathna@gmail.com>
2025-11-02 13:26:39 +0530

    drizzle-orm-client checked

--- packages/drizzle-orm-client/src/schema/index.ts
+++ packages/drizzle-orm-client/src/schema/index.ts
@@ -3,5 +3,4 @@ export * from './permissions';
 export * from './roles';
 export * from './role-permissions';
 export * from './user-permissions';
-export * from './settings';
 export * from './documents';
